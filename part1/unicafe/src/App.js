285c3897b802119720ca55971f6820e91fbed734
Josué <d4n11083@estudiantec.cr>
2021-09-06 21:20:11 -0600

    Unicafe -- Finished

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -9,7 +9,13 @@ const Button = ({onClick, text}) => {
 
 const StatisticLine = ({text, value}) => {
   return (
-      <p>{text}:{value} </p>
+    <tbody>
+      <tr>
+        <td>{text}</td>
+        <td>{value}</td>
+      </tr>
+    </tbody>
+    
   )
 } 
 
@@ -25,12 +31,15 @@ const Statistics = ({good, neutral, bad}) => {
   return (
     <div>
       <h1>statistics</h1>
-      <StatisticLine text="good" value={good}/>
-      <StatisticLine text="neutral" value={neutral}/>
-      <StatisticLine text="bad" value={bad}/>
-      <StatisticLine text="all" value={good + neutral + bad}/>
-      <StatisticLine text="average" value={(good + neutral + bad)/3}/>
-      <StatisticLine text="positive" value={(good/(good+neutral+bad))*100}/>
+      <table>
+       <StatisticLine text="good" value={good}/>
+       <StatisticLine text="neutral" value={neutral}/>
+       <StatisticLine text="bad" value={bad}/>
+       <StatisticLine text="all" value={good + neutral + bad}/>
+       <StatisticLine text="average" value={(good + neutral + bad)/3}/>
+       <StatisticLine text="positive" value={(good/(good+neutral+bad))*100}/>
+      </table>
+
     </div>
   )
 
