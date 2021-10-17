c49d54e58f4a6cbc949a3396fef57e5acdbd2cf5
Josué <d4n11083@estudiantec.cr>
2021-10-17 07:04:20 -0600

    2.11 Phonebook step 6, retrieving data from server
    added an effect hook to do a axios.get of the persons data from db.json

--- part2/thephonebook/src/App.js
+++ part2/thephonebook/src/App.js
@@ -1,21 +1,28 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Person from './Components/Person'
 import SearchFilter from './Components/SearchFilter'
 import PersonForm from './Components/PersonForm'
 import Persons from './Components/Persons'
-
+import axios from 'axios'
 const App = () => {
   //UseStates for "Variables"
  // const [ persons, setPersons ] = useState([])
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([]) //Empty because we want to retirve the info from the server 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ searchFilter, setSearchFilter ] = useState('')
+
+  const GETServerNotes = () =>{
+    console.log("Sending request");
+    axios.get('http://localhost:3001/persons')
+      .then( personList => {
+        console.log("Request Fulfilled");
+        const people = personList.data
+        setPersons(people)
+      } )
+  }
+
+  useEffect(GETServerNotes,[]) //Empy [] so it runs after th first render
 
   
   //Add a new person with name from the form's input, if the name doesn't exists already
